5e46615956410de1f0eb085a8e5ced8b9b14991f
Jan <jan.gasser@bluewin.ch>
2017-06-28 19:03:02 +0200

    Create Lightbox design with functionality. Design navbar and meme-posts layout. Merge Cedi-Memes together.

--- resources/main.js
+++ resources/main.js
@@ -1,15 +1,38 @@
 window.onload = function() {
 }
 
-function loadContent() {
-  var folder = 'images/*';
+function openModal() {
+  document.body.style.overflow = "hidden";
+  document.getElementById('myModal').style.display = "block";
+}
 
-  $.ajax({
-    url : folder,
-    success: function(data) {
-      $(data).find("a").attr("href", function (i, value) {
-        $("#content-frame").append("<img src='" + folder + value + "'>");
-      })
-    }
-  });
+function closeModal() {
+  document.body.style.overflow = "auto"
+  document.getElementById('myModal').style.display = "none";
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
